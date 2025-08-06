d8784b5feb6ba6e063b9b21a11bc102a1e4071bb
SagarTeotia1 <sagar1teotia@gmail.com>
2025-08-06 11:48:38 +0530

    commit

--- teaching-platform/next.config.ts
+++ teaching-platform/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
-
+module.exports = {
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+}
 const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
